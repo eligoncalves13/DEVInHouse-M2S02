077cdf067d893d057f55b8b3b52de74c4dfa67a5
Élida <elidabeatriz13@gmail.com>
2022-03-14 18:55:23 -0300

    add ex10

---
+++ Ex 10 - Notas da turma/Program.cs
@@ -0,0 +1,36 @@
+﻿static double GradeAverage(double[] numbers){
+    double avarage;
+    double sum = 0;
+    int count = 0;
+            
+    for(int i = 0; i < 30; i++)
+        {             
+            sum += numbers[i];
+            count++;
+        }
+
+    avarage = sum/count;
+
+    return avarage;
+}
+
+double[] numbers = new double[30];
+Random random = new Random();
+double classAvarage;
+
+Console.WriteLine("Notas da turma: ");
+for(int i = 0; i < 30; i++)
+    {
+        double randomNumber = random.Next(0, 11);
+        numbers[i] = randomNumber;
+        Console.WriteLine(numbers[i]);
+    }
+
+classAvarage = GradeAverage(numbers);
+
+Console.WriteLine($"Média da Turma: {classAvarage:F2}");
+
+double maxValue = numbers.Max();
+Console.WriteLine($"Maior nota da turma: {maxValue}");
+double minValue = numbers.Min();
+Console.WriteLine($"Menor nota da turma: {minValue}");
---
+++ Ex 10 - Notas da turma/readme.md
@@ -0,0 +1,5 @@
+# Ex 10 - notas da turma
+
+Salve dev, blz?
+
+Crie uma função que recebe um array com as 30 notas da turma. Exiba na tela a maior nota, a menor nota e a média da turma.
