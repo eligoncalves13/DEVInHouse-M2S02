fe3f5deb3d101de4d20d685561b87abf9acd9a93
Élida <elidabeatriz13@gmail.com>
2022-03-14 18:38:06 -0300

    add ex6

---
+++ Ex 6 - Truques com strings/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Digite seu nome: ");
+string name = Console.ReadLine();
+
+string nameReversed = new string(name.Reverse().ToArray());
+Console.WriteLine($"Nome invertido: {nameReversed}");
+Console.WriteLine($"Total de letras: {name.Length}");
---
+++ Ex 6 - Truques com strings/readme.md
@@ -0,0 +1,5 @@
+# Ex 6 - Truques com strings
+
+Salve dev, blz?
+
+Crie um programa que peça ao usuário que digite seu nome, e em seguida escreva na tela o nome de trás pra frente e mostre o total de letras que o nome possui.
