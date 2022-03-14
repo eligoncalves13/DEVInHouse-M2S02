584796f1b863f914ca01cbb52ec84a20a2aa5a9d
Élida <elidabeatriz13@gmail.com>
2022-03-14 18:40:51 -0300

    add ex7

---
+++ Ex 7 - Média das notas/Program.cs
@@ -0,0 +1,18 @@
+﻿double[] grades = new double[4];
+
+for(int i = 1; i <= 3; i++) {
+    Console.WriteLine($"Informe a nota {i}");
+    grades[i] = Convert.ToDouble(Console.ReadLine());
+}
+
+double sum = 0;
+int count = 0;
+
+for(int i = 1; i <= 3; i++) {
+    sum += grades[i];
+    count++;
+}
+
+double avarage = sum/count;
+
+Console.WriteLine($"Sua média final é {avarage:F2}");
---
+++ Ex 7 - Média das notas/readme.md
@@ -0,0 +1,5 @@
+# Ex 7 - Média das notas
+
+Salve dev, blz?
+
+Crie um programa que peça ao usuário para inserir as notas do semestre, e deve retornar a média final. O programa deve pedir 3 notas, que devem ser números decimais, e retornar um número com 2 casas depois da vírgula, com o texto “Sua média final é:” seguido do valor.
