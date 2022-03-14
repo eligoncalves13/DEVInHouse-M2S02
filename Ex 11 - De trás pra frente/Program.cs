12eceacc5cd04abe5d1e1b7080f84928a2592280
Élida <elidabeatriz13@gmail.com>
2022-03-14 19:09:56 -0300

    add ex11

---
+++ Ex 11 - De trás pra frente/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+int count = 0;
+Console.WriteLine("Informe quantos números deseja inserir no array: ");
+count = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+int[] numbers =  new int[count];
+
+if(count > 0){
+    while(i < count){
+        Console.WriteLine($"Informe o número {i+1}: ");
+        numbers[i] = Convert.ToInt32(Console.ReadLine());
+        i++;
+    }
+
+    Console.WriteLine("Números ao digitados: ");
+    for(int j = 0; j < count; j++){
+        Console.WriteLine(numbers[j]);
+}
+    Console.WriteLine("Números ao contrário: ");
+    for(int k = count-1; k >= 0; k--){
+        Console.WriteLine(numbers[k]);
+    }
+}
+ 
+
+        
+
+
---
+++ Ex 11 - De trás pra frente/readme.md
@@ -0,0 +1,11 @@
+# Ex 11 - De trás pra frente
+
+Salve dev, blz?
+
+Crie um programa que pergunte ao usuário quantos números ele deseja inserir no array.
+
+Depois pergunte esses números ao usuário, um de cada vez, e coloque em um array. Depois exiba tal array.
+
+Modifique os elementos do array de modo a a sequência de números ficar do contrário.
+Ou seja, se digitou: 1,2,3
+Vai aparecer: 3,2,1
