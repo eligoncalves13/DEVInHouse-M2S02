8fc178b82a95846b293f7798ffa956dbe61d81a3
Élida <elidabeatriz13@gmail.com>
2022-03-14 18:43:35 -0300

    add ex8

---
+++ Ex 8 - Fatorial/Program.cs
@@ -0,0 +1,13 @@
+﻿int number;
+int factorial = 1;
+
+do {
+    Console.WriteLine("Digite um número inteiro de 0 a 10: ");
+    number = Convert.ToInt32(Console.ReadLine());
+} while(number < 0 || number > 10);
+
+for(int i = number; i > 1; i--) {
+    factorial *= i;
+}
+
+Console.WriteLine($"Fatorial de {number} é {factorial}");
---
+++ Ex 8 - Fatorial/readme.md
@@ -0,0 +1,5 @@
+# Ex 8 - Fatorial
+
+Salve dev, blz?
+
+Crie um programa que calcule o fatorial de um número inteiro de 0 a 10. O programa deve pedir para o usuário inserir um número inteiro, mas só deve aceitar valores de 0 a 10, e então deve calcular o fatorial desse número e escrever o resultado na tela.
