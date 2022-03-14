2f63129bdec61acccb1524ab52c53776d0771417
Élida <elidabeatriz13@gmail.com>
2022-03-14 18:20:00 -0300

    add ex3

---
+++ Ex 3 - Nome completo/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Informe seu nome:");
+string name = Console.ReadLine();
+Console.WriteLine("Informe seu sobrenome:");
+string lastName = Console.ReadLine();
+
+string fullName = $"O nome completo do usuário é {name} {lastName}";
+Console.WriteLine(fullName);
---
+++ Ex 3 - Nome completo/readme.md
@@ -0,0 +1,5 @@
+# Ex 3 - Nome completo
+
+Salve dev, blz?
+
+Crie um programa que pergunte ao usuário seu nome, depois pergunte seu sobrenome, e então escreva na tela, em uma única “string”, o nome completo do usuário.
