a8410a0e545dca41eeb471df434b28fd8586084c
Élida <elidabeatriz13@gmail.com>
2022-03-14 18:28:34 -0300

    add ex4

---
+++ Ex 4 - Variáveis parte 2/Program.cs
@@ -0,0 +1,7 @@
+﻿var nota1 = 8;
+var nota2 = 5;
+var nota3 = 6;
+
+var media = (Double)(nota1 + nota2 + nota3 ) / 3;
+
+Console.Write($"O valor da variável média é {media:F}");
---
+++ Ex 4 - Variáveis parte 2/readme.md
@@ -0,0 +1,9 @@
+# Ex 4 - Variáveis parte 2
+
+var nota1 = 8;
+var nota2 = 5;
+var nota3 = 6;
+
+var media = ( nota1 + nota2 + nota3 ) /3 ;
+
+Qual o valor da variável "media"?
