8deb4e957a3169a3d906723188302258adc64566
Élida <elidabeatriz13@gmail.com>
2022-03-14 18:34:57 -0300

    add ex5

---
+++ Ex 5 - Arredondando números/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Informe um número com 2 casas depois da vírgula: ");
+double numberDouble = Convert.ToDouble(Console.ReadLine());
+
+int numberInt = Convert.ToInt32(numberDouble);
+
+Console.WriteLine(numberInt);
---
+++ Ex 5 - Arredondando números/readme.md
@@ -0,0 +1,5 @@
+# Ex 5 - arredondando números
+
+Salve dev, blz?
+
+Crie um programa que pergunte um número com 2 casas depois da vírgula, converta esse número para um inteiro e escreva o resultado na tela.
